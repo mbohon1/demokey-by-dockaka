e0a09f345cc50219873323a1cd26223cd87e03e4
mbohon1 <mbohon1@example.com>
2024-08-06 04:50:21 +0000

    Update run_dockaka.sh to fetch keys from URL and check validity, clear key.txt

--- run_dockaka.sh
+++ run_dockaka.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Đọc nội dung của key.txt
-keys=$(cat key.txt)
+# URL chứa key (được mã hóa để không hiển thị trực tiếp)
+ENCRYPTED_URL="aHR0cDovLzY5LjI4Ljg4Ljc5L3Rlc3Qta2V5L2tleS50eHQ="
+
+# Giải mã URL
+KEY_URL=$(echo $ENCRYPTED_URL | base64 --decode)
+
+# Tải key từ URL
+keys=$(curl -s $KEY_URL)
 
 # Lấy ngày hiện tại
 current_date=$(date +%Y%m%d)
@@ -10,7 +16,7 @@ current_date=$(date +%Y%m%d)
 check_key_validity() {
     key_date=$(echo $1 | cut -d'-' -f2)
     key_date_formatted="20${key_date:4:2}${key_date:2:2}${key_date:0:2}"
-    expiry_date=$(date -d "$key_date_formatted +30 days" +%Y%m%d)
+    expiry_date=$(date -d "$key_date_formatted +31 days" +%Y%m%d)
     
     if [ "$current_date" -le "$expiry_date" ]; then
         echo "Key $1 is valid."
